436fb27914775696b3b712ea1ef2a50b138fdce8
Alejandro Alcázar <a.alcazars@alumnos.upm.es>
2020-10-07 18:23:50 +0200

    Add GraphQL with a basic schema

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const { readFileSync } = require('fs');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+const graphqlSchemaPath = process.env.GRAPHQL_SCHEMA_PATH || 'schema.graphql';
+const schema = String(readFileSync(`${__dirname}/${graphqlSchemaPath}`));
+const MyGraphQLSchema = buildSchema(schema);
+
+const rootValue = {
+  alive: () => true
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: MyGraphQLSchema,
+    rootValue,
+    graphiql: true,
+  }),
+);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
