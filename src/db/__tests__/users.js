fb0c3aad63a640c7e25a2d44f5e8460c1940f076
Alejandro Alcázar <a.alcazars@alumnos.upm.es>
2020-11-26 20:52:49 +0100

    Fix lint

--- src/db/__tests__/users.js
+++ src/db/__tests__/users.js
@@ -64,11 +64,10 @@ describe('Users database methods', () => {
   });
 
   describe('verify', () => {
-
     beforeEach(() => Promise.all([
-        getDatabase('users'),
-        argon2.hash('baz'),
-        argon2.hash('qux'),
+      getDatabase('users'),
+      argon2.hash('baz'),
+      argon2.hash('qux'),
     ])
       .then(([users, bazPassword, quxPassword]) => users.insertMany([
         { _id: ObjectId('111111111111111111111111'), name: 'foo', password: bazPassword },
@@ -76,83 +75,69 @@ describe('Users database methods', () => {
       ])));
 
     it('Should return 111111111111111111111111 user if provided foo name and baz password', () => new Promise((resolve, reject) => {
-
-        verify('foo', 'baz')
-            .then((retrievedUser) => {
-                expect(retrievedUser).toHaveProperty('_id', ObjectId('111111111111111111111111'));
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-
+      verify('foo', 'baz')
+        .then((retrievedUser) => {
+          expect(retrievedUser).toHaveProperty('_id', ObjectId('111111111111111111111111'));
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }));
 
     it('Should return 222222222222222222222222 user if provided foo name and qux password', () => new Promise((resolve, reject) => {
-
-        verify('bar', 'qux')
-            .then((retrievedUser) => {
-                expect(retrievedUser).toHaveProperty('_id', ObjectId('222222222222222222222222'));
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-
+      verify('bar', 'qux')
+        .then((retrievedUser) => {
+          expect(retrievedUser).toHaveProperty('_id', ObjectId('222222222222222222222222'));
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }));
 
     it('Should return false if provided foo name and qux password', () => new Promise((resolve, reject) => {
-
-        verify('foo', 'qux')
-            .then((retrievedUser) => {
-                expect(retrievedUser).toBe(false);
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-
+      verify('foo', 'qux')
+        .then((retrievedUser) => {
+          expect(retrievedUser).toBe(false);
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }));
 
     it('Should return false if provided foo name and quux password', () => new Promise((resolve, reject) => {
-
-        verify('foo', 'quux')
-            .then((retrievedUser) => {
-                expect(retrievedUser).toBe(false);
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-
+      verify('foo', 'quux')
+        .then((retrievedUser) => {
+          expect(retrievedUser).toBe(false);
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }));
 
     it('Should return false if provided quux name', () => new Promise((resolve, reject) => {
-
-        verify('quux', 'qux')
-            .then((retrievedUser) => {
-                expect(retrievedUser).toBe(false);
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-
+      verify('quux', 'qux')
+        .then((retrievedUser) => {
+          expect(retrievedUser).toBe(false);
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }));
 
     it('Should return false if provided no arguments', () => new Promise((resolve, reject) => {
-
-        verify()
-            .then((retrievedUser) => {
-                expect(retrievedUser).toBe(false);
-                resolve();
-            })
-            .catch((error) => {
-                reject(error);
-            });
-
+      verify()
+        .then((retrievedUser) => {
+          expect(retrievedUser).toBe(false);
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }));
-
   });
-
 });
