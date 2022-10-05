ce56e2d461aeab1c2de5edbbf780bcaf42f5530e
PuppedToy <mapastzar@hotmail.com>
2022-10-05 22:18:10 +0200

    Update some forge works that I can't remember rn

--- src/data/forges/conditions.js
+++ src/data/forges/conditions.js
@@ -1,49 +1,67 @@
 const conditions = {
-  amountCardsBelowThreshold: {
-    name: 'The amount of eligible cards is below a threshold',
+  cards: {
+    inTargetPlace: {
+      name: 'inTargetPlace',
+      arguments: {
+        place: {
+          type: 'place',
+        },
+      },
+      description: 'is in the target place',
+      text: 'is in $place',
+    },
   },
-  amountCardsAboveThreshold: {
-    name: 'The amount of eligible cards is above a threshold',
-  },
-  amountCardsEqualsThreshold: {
-    name: 'The amount of eligible cards equals a threshold',
-  },
-  amountKingdomsBelowThreshold: {
-    name: 'The amount of eligible kingdoms is below a threshold',
-  },
-  amountKingdomsAboveThreshold: {
-    name: 'The amount of eligible kingdoms is above a threshold',
-  },
-  amountKingdomsEqualsThreshold: {
-    name: 'The amount of eligible kingdoms equals a threshold',
-  },
-  amountCardStatsSumBelowThreshold: {
-    name: 'The sum of one stat among the eligible cards is below the threshold',
-  },
-  amountCardStatsSumAboveThreshold: {
-    name: 'The sum of one stat among the eligible cards is above the threshold',
-  },
-  amountCardStatsSumEqualsThreshold: {
-    name: 'The sum of one stat among the eligible cards equals a threshold',
-  },
-  amountKingdomStatsSumBelowThreshold: {
-    name: 'The sum of one stat among the eligible kingdoms is below the threshold',
-  },
-  amountKingdomStatsSumAboveThreshold: {
-    name: 'The sum of one stat among the eligible kingdoms is above the threshold',
-  },
-  amountKingdomStatsSumEqualsThreshold: {
-    name: 'The sum of one stat among the eligible kingdoms equals a threshold',
-  },
-  turnCountBelowThreshold: {
-    name: 'The turn count is below a threshold',
-  },
-  turnCountAboveThreshold: {
-    name: 'The turn count is above a threshold',
-  },
-  turnCountEqualsThreshold: {
-    name: 'The turn count equals a threshold',
+  kingdoms: {
+    // @TODO
   },
 };
+
+// const conditions = {
+//   amountCardsBelowThreshold: {
+//     name: 'The amount of eligible cards is below a threshold',
+//   },
+//   amountCardsAboveThreshold: {
+//     name: 'The amount of eligible cards is above a threshold',
+//   },
+//   amountCardsEqualsThreshold: {
+//     name: 'The amount of eligible cards equals a threshold',
+//   },
+//   amountKingdomsBelowThreshold: {
+//     name: 'The amount of eligible kingdoms is below a threshold',
+//   },
+//   amountKingdomsAboveThreshold: {
+//     name: 'The amount of eligible kingdoms is above a threshold',
+//   },
+//   amountKingdomsEqualsThreshold: {
+//     name: 'The amount of eligible kingdoms equals a threshold',
+//   },
+//   amountCardStatsSumBelowThreshold: {
+//     name: 'The sum of one stat among the eligible cards is below the threshold',
+//   },
+//   amountCardStatsSumAboveThreshold: {
+//     name: 'The sum of one stat among the eligible cards is above the threshold',
+//   },
+//   amountCardStatsSumEqualsThreshold: {
+//     name: 'The sum of one stat among the eligible cards equals a threshold',
+//   },
+//   amountKingdomStatsSumBelowThreshold: {
+//     name: 'The sum of one stat among the eligible kingdoms is below the threshold',
+//   },
+//   amountKingdomStatsSumAboveThreshold: {
+//     name: 'The sum of one stat among the eligible kingdoms is above the threshold',
+//   },
+//   amountKingdomStatsSumEqualsThreshold: {
+//     name: 'The sum of one stat among the eligible kingdoms equals a threshold',
+//   },
+//   turnCountBelowThreshold: {
+//     name: 'The turn count is below a threshold',
+//   },
+//   turnCountAboveThreshold: {
+//     name: 'The turn count is above a threshold',
+//   },
+//   turnCountEqualsThreshold: {
+//     name: 'The turn count equals a threshold',
+//   },
+// };
 
 module.exports = conditions;
