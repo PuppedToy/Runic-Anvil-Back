56afbff2581357da460b15f2ba50067d4b98d66a
PuppedToy <alcazar.sacris@gmail.com>
2023-01-12 17:10:43 +0100

    Card update & findOneWithoutImage tests

--- src/db/__tests__/cards.js
+++ src/db/__tests__/cards.js
@@ -1,6 +1,11 @@
 const { ObjectId } = require('mongodb');
 
-const { getById, create } = require('../cards');
+const {
+  getById,
+  create,
+  findOneWithoutImage,
+  update,
+} = require('../cards');
 const { getDatabase, closeDatabase, connectDatabase } = require('../../utils/getDatabase');
 const emptyDatabase = require('../../utils/testing/emptyDatabase');
 
@@ -66,6 +71,66 @@ describe('Cards database methods', () => {
 
     it('Should throw an error if card is not provided', async () => {
       await expect(create()).rejects.toThrow();
+    });
+  });
+
+  describe('findOneWithoutImage', () => {
+    it('Should find the card that has no image', async () => {
+      await cardsDb.insertMany([
+        { _id: ObjectId('311111111111111111111111'), name: 'foo', image: 'fooImage' },
+        { _id: ObjectId('322222222222222222222222'), name: 'bar', image: null },
+      ]);
+
+      const card = await findOneWithoutImage();
+      expect(card).toHaveProperty('name', 'bar');
+    });
+
+    it('Should return null if every card has image', async () => {
+      await cardsDb.insertMany([
+        { _id: ObjectId('331111111111111111111111'), name: 'foo', image: 'fooImage' },
+        { _id: ObjectId('332222222222222222222222'), name: 'bar', image: 'barImage' },
+      ]);
+
+      const card = await findOneWithoutImage();
+      expect(card).toBeNull();
+    });
+  });
+
+  describe('update', () => {
+    beforeEach(async () => {
+      await cardsDb.insertOne({
+        _id: ObjectId('411111111111111111111111'),
+        name: 'foo',
+      });
+    });
+
+    it('Should throw if card is not passed', async () => {
+      await expect(update('411111111111111111111111')).rejects.toThrow();
+    });
+
+    it('Should not update _id field', async () => {
+      await update('411111111111111111111111', { _id: 'newId' });
+      const card = await cardsDb.findOne({ _id: ObjectId('411111111111111111111111') });
+      expect(card).toHaveProperty('_id', ObjectId('411111111111111111111111'));
+    });
+
+    it('Should return null if the card does not exist', async () => {
+      const updatedCount = await update('444444444444444444444444', { name: 'newName' });
+      expect(updatedCount).toBe(0);
+    });
+
+    it('Should update a previous field of the card', async () => {
+      const updatedCount = await update('411111111111111111111111', { name: 'newName' });
+      const updatedCard = await cardsDb.findOne({ _id: ObjectId('411111111111111111111111') });
+      expect(updatedCount).toBe(1);
+      expect(updatedCard).toHaveProperty('name', 'newName');
+    });
+
+    it('Should create a new field of the card', async () => {
+      const updatedCount = await update('411111111111111111111111', { newField: 'newField' });
+      const updatedCard = await cardsDb.findOne({ _id: ObjectId('411111111111111111111111') });
+      expect(updatedCount).toBe(1);
+      expect(updatedCard).toHaveProperty('newField', 'newField');
     });
   });
 });
