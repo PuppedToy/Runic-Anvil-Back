d29b46b630135592b7239fdf84ce8b950e04de7b
PuppedToy <alcazar.sacris@gmail.com>
2023-01-16 09:21:15 +0100

    Finish search method

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -199,5 +199,304 @@ describe('API', () => {
         expect(response.body).toHaveProperty('data');
         expect(response.body).toHaveProperty('pagination');
       }));
+
+    it('Should return 200 if requesting with name', () => request(app)
+      .get('/api/cards/search?name=bar')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 500 if found an error', () => request(app)
+      .get('/api/cards/search?name=error')
+      .then((response) => {
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 404 if found no results', () => request(app)
+      .get('/api/cards/search?name=empty')
+      .then((response) => {
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with limit', () => request(app)
+      .get('/api/cards/search?limit=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 200 if requesting with offset', () => request(app)
+      .get('/api/cards/search?offset=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 200 if requesting with ignoreImage', () => request(app)
+      .get('/api/cards/search?ignoreImage=true')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with wrong ignoreImage', () => request(app)
+      .get('/api/cards/search?ignoreImage=foo')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with type creature', () => request(app)
+      .get('/api/cards/search?type=creature')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 200 if requesting with type spell', () => request(app)
+      .get('/api/cards/search?type=spell')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 200 if requesting with type weapon', () => request(app)
+      .get('/api/cards/search?type=weapon')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with type invalid', () => request(app)
+      .get('/api/cards/search?type=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with unitType human', () => request(app)
+      .get('/api/cards/search?unitType=human')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 200 if requesting with unitType beast', () => request(app)
+      .get('/api/cards/search?unitType=beast')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 200 if requesting with unitType invalid', () => request(app)
+      .get('/api/cards/search?unitType=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with forge addPassiveEffect', () => request(app)
+      .get('/api/cards/search?forge=addPassiveEffect')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with forge invalid', () => request(app)
+      .get('/api/cards/search?forge=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with cost 1', () => request(app)
+      .get('/api/cards/search?cost=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with cost invalid', () => request(app)
+      .get('/api/cards/search?cost=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with minCost 1', () => request(app)
+      .get('/api/cards/search?minCost=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with minCost invalid', () => request(app)
+      .get('/api/cards/search?minCost=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with maxCost 1', () => request(app)
+      .get('/api/cards/search?maxCost=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with maxCost invalid', () => request(app)
+      .get('/api/cards/search?maxCost=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with attack 1', () => request(app)
+      .get('/api/cards/search?attack=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with attack invalid', () => request(app)
+      .get('/api/cards/search?attack=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with minAttack 1', () => request(app)
+      .get('/api/cards/search?minAttack=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with minAttack invalid', () => request(app)
+      .get('/api/cards/search?minAttack=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with maxAttack 1', () => request(app)
+      .get('/api/cards/search?maxAttack=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with maxAttack invalid', () => request(app)
+      .get('/api/cards/search?maxAttack=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with hp 1', () => request(app)
+      .get('/api/cards/search?hp=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with hp invalid', () => request(app)
+      .get('/api/cards/search?hp=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with minHp 1', () => request(app)
+      .get('/api/cards/search?minHp=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with minHp invalid', () => request(app)
+      .get('/api/cards/search?minHp=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with maxHp 1', () => request(app)
+      .get('/api/cards/search?maxHp=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with maxHp invalid', () => request(app)
+      .get('/api/cards/search?maxHp=invalid')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 400 if requesting with unknown property', () => request(app)
+      .get('/api/cards/search?unknown=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
+
+    it('Should return 200 if requesting with name and unitType', () => request(app)
+      .get('/api/cards/search?name=1&unitType=human')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).toHaveProperty('pagination');
+      }));
+
+    it('Should return 400 if requesting with name and unknown property', () => request(app)
+      .get('/api/cards/search?name=1&unknown=1')
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('message');
+      }));
   });
 });
--- src/restApi/cards/cards.router.js
+++ src/restApi/cards/cards.router.js
@@ -3,11 +3,30 @@ const { validate, Joi } = require('../../utils/joiExtended');
 
 const controller = require('./cards.controller');
 
+const cardTypes = require('../../data/forges/cardTypes');
+const unitTypes = require('../../data/forges/unitTypes');
+const { forgeGenerators } = require('../../lib/forge/generateForge');
+
 const router = express.Router();
 
 const searchValidator = {
   query: Joi.object({
     name: Joi.string(),
+    ignoreImage: Joi.boolean(),
+    type: Joi.string().valid(...cardTypes),
+    unitType: Joi.string().valid(...Object.keys(unitTypes)),
+    forge: Joi.string().valid(...forgeGenerators.map((forge) => forge.type)),
+    cost: Joi.number().integer().min(0),
+    minCost: Joi.number().integer().min(0),
+    maxCost: Joi.number().integer().min(0),
+    attack: Joi.number().integer().min(0),
+    minAttack: Joi.number().integer().min(0),
+    maxAttack: Joi.number().integer().min(0),
+    hp: Joi.number().integer().min(0),
+    minHp: Joi.number().integer().min(0),
+    maxHp: Joi.number().integer().min(0),
+    limit: Joi.number().integer().min(1),
+    offset: Joi.number().integer().min(0),
   }),
 };
 
