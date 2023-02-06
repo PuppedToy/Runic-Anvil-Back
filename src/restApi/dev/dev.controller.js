48539e4849576eb7f0157145e47772484652452e
PuppedToy <alcazar.sacris@gmail.com>
2023-02-06 16:05:39 +0100

    Bulk update & custom query DEV endpoints

--- src/restApi/dev/dev.controller.js
+++ src/restApi/dev/dev.controller.js
@@ -1,4 +1,5 @@
 const { generateCard } = require('../../lib/generateCard');
+const { bulkUpdate, customQuery } = require('../../db/cards');
 
 function simulateGenerateCardController(req, res, next) {
   const { level = 1 } = req.body;
@@ -15,6 +16,38 @@ function simulateGenerateCardController(req, res, next) {
   }
 }
 
+async function customQueryController(req, res, next) {
+  const { query } = req.body;
+
+  try {
+    const result = await customQuery(query);
+
+    return res.status(200).json({
+      message: 'Query executed',
+      result,
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function bulkUpdateController(req, res, next) {
+  const { query, stringUpdateCardMethod } = req.body;
+
+  try {
+    const result = await bulkUpdate(query, stringUpdateCardMethod);
+
+    return res.status(200).json({
+      message: 'Cards updated',
+      result,
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
+
 module.exports = {
   simulateGenerateCardController,
+  customQueryController,
+  bulkUpdateController,
 };
--- src/restApi/dev/dev.router.js
+++ src/restApi/dev/dev.router.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { validate, Joi } = require('express-validation');
+const { validate, Joi } = require('../../utils/joiExtended');
 
 const controller = require('./dev.controller');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 const simulateGenerateCardValidator = {
   body: Joi.object({
@@ -11,10 +15,35 @@ const simulateGenerateCardValidator = {
   }),
 };
 
+const customQueryValidator = {
+  body: Joi.object({
+    query: Joi.object().required(),
+  }),
+};
+
+const bulkUpdateValidator = {
+  body: Joi.object({
+    query: Joi.object().required(),
+    stringUpdateCardMethod: Joi.string().regex(/^\s*function\s+updateCard\s*?\(\w+\)\s*\{[^ä]+\}\s*$/).required(),
+  }),
+};
+
 router.post(
   '/simulate/generate-card',
   validate(simulateGenerateCardValidator),
   controller.simulateGenerateCardController,
+);
+
+router.post(
+  '/custom-query',
+  validate(customQueryValidator),
+  controller.customQueryController,
+);
+
+router.put(
+  '/bulk-update',
+  validate(bulkUpdateValidator),
+  controller.bulkUpdateController,
 );
 
 module.exports = router;
